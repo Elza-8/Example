170e47a66c3c6ff8656fe47874c3632c856e645c
Elza-8 <dulieva-xxl@mail.ru>
2022-10-14 12:34:43 +0500

    add example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "эльза")
+{
+    Console.WriteLine("Ура, это же ЭЛЬЗА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
